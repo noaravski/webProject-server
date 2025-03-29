f30d593f59ed34a9153cdd7fcd06321bde214da7
idan efraim <idan1000011@walla.com>
2025-03-29 14:27:06 +0300

    update ai gen

--- src/controllers/ai_controller.ts
+++ src/controllers/ai_controller.ts
@@ -7,7 +7,7 @@ const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
 const enhanceReview = async (req: Request, res: Response) => {
   const reviewContent = req.body.content;
-  const promt = `I'm sending you a json with a text field, please make the text more proffesional, and respond with a json with a text field inside it with the more proffesional text {text: "${reviewContent}"}`;
+  const promt = `I'm sending you a json with a text field, make a positive recommendation about the movie name - ${reviewContent}. generate up to 300 characters, if the movie exists respond with a json with a text field inside it with your answer {text: "${reviewContent}"}. if the movie does not exists please return i dont know this film`;
 
   const result = await model.generateContent({
     contents: [{ role: "user", parts: [{ text: promt }] }],
